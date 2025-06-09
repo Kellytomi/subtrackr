40fdecba9951dd6cfc0442edf1186d24ebfac444
Etoma-etoto Odi <kelvinetoma95@gmail.com>
2025-06-09 07:25:55 +0100

    🔖 Update Android version to 1.0.5+7 for new Shorebird release - Sync Android build.gradle.kts with pubspec.yaml version - Fixes Shorebird version conflicts - Enables clean release deployment

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -62,8 +62,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.0.4"
+        versionCode = 7
+        versionName = "1.0.5"
         multiDexEnabled = true
     }
 
